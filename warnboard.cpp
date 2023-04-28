493d23c205d737503440a7bbb61c0d4ad608059c
Li MingZhou <leemz@nuaa.edu.cn>
2023-04-28 10:07:15 +0800

    delete useless words

--- warnboard.cpp
+++ warnboard.cpp
@@ -1,9 +1,4 @@
 
-/*
- * Copyright (c) Huawei Technologies Co., Ltd. 2020-2020. All rights reserved.
- * Description: 上机编程认证
- * Note: 缺省代码仅供参考，可自行决定使用、修改或删除
- */
 #include <iostream>
 #include <string>
 #include <vector>
